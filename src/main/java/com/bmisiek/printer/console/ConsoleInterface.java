99205f328b7f9d84ac0bd05c1827962d94c5a162
Indeed <gravitybartek@gmail.com>
2025-04-08 11:10:51 +0200

    wip action getting

--- src/main/java/com/bmisiek/printer/console/ConsoleInterface.java
+++ src/main/java/com/bmisiek/printer/console/ConsoleInterface.java
@@ -1,20 +1,22 @@
 package com.bmisiek.printer.console;
 
-import com.bmisiek.game.dungeon.Dungeon;
 import com.bmisiek.game.dungeon.DungeonInterface;
 import com.bmisiek.game.event.DamageTakenEvent;
 import com.bmisiek.game.event.DungeonEnteredEvent;
 import com.bmisiek.game.player.Player;
 import com.bmisiek.printer.console.printers.DungeonPrinter;
+import com.bmisiek.printer.contract.ActionNotFoundException;
 import com.bmisiek.printer.contract.GameAction;
 import com.bmisiek.printer.contract.GuiInterface;
 import org.jetbrains.annotations.NotNull;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Service;
 
 import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
 
 @Service
 public class ConsoleInterface implements GuiInterface, ApplicationListener<ApplicationEvent> {
@@ -41,14 +43,44 @@ public class ConsoleInterface implements GuiInterface, ApplicationListener<Appli
         SeparateUpdates();
     }
 
+    private static final HashMap<String, GameAction> actions = new HashMap<>() {{
+        put("down", GameAction.MOVE_DOWN);
+        put("up", GameAction.MOVE_UP);
+        put("right", GameAction.MOVE_RIGHT);
+        put("left", GameAction.MOVE_LEFT);
+    }};
+
     public GameAction Act(DungeonInterface dungeon, Player player) throws RuntimeException {
-        try {
-            var command = System.in.read();
-        } catch(Exception e) {
-            throw new RuntimeException(e);
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                return GetAction(scanner);
+            } catch (ActionNotFoundException e) {
+                System.out.println(e.getMessage());
+            } catch (Exception e) {
+                System.out.println("Technical Error");
+            }
+        }
+    }
+
+    private static GameAction GetAction(Scanner scanner) throws ActionNotFoundException {
+        var command = scanner.nextLine();
+
+        //TODO: Possible parameters in the future
+        var commandName = Arrays.stream(command.split(" ")).findFirst().orElseThrow();
+        AssertValidCommand(commandName);
+
+        return actions.get(commandName);
+    }
+
+    private static void AssertValidCommand(String commandName) throws ActionNotFoundException {
+        if(commandName == null || commandName.isEmpty()){
+            throw new ActionNotFoundException("Empty command");
         }
 
-
+        if(!actions.containsKey(commandName)){
+            throw new ActionNotFoundException("Invalid command");
+        }
     }
 
     private void printLine(String line) {
